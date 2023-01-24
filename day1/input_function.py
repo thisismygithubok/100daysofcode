f2c3cf639e3e370428fea5b44328406da03f341f
Steven Schmidt <steven@thisismynameok.com>
2023-01-24 16:18:00 -0500

    add input_function.py

---
+++ day1/input_function.py
@@ -0,0 +1,14 @@
+# This is an input example, i.e. a prompt for user to input data
+    # input("What is your name? : ")
+
+# Input w/ automatic print after user input
+    # print("Hello, " + input("What is your name? : ") + "!")
+
+# Take user's name/string input and print our character count
+    # print(len(input("What is your name? ")))
+
+# Here's how you'd store these into variables to make it more concise
+name = input("What is your name? ")
+length = len(name)
+
+print(length)
