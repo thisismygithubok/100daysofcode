fb52afe91fa9157bfcf9c1d834df4a6bf6fb19a6
Steven Schmidt <steven@thisismynameok.com>
2023-01-25 12:19:10 -0500

    change name of data type example file to new format as well as adding mathematical operations example

---
+++ day2/example_mathematical_operations.py
@@ -0,0 +1,22 @@
+#Other Mathematical Operations
+
+3+5 #Addition
+3-5 #Subtraction
+3*5 #Multiplication
+3/5 #Division
+3**5 #Exponents, i.e. 3^5
+
+print(3**5)
+
+#PEMDAS
+# ()
+# **
+# * or /
+# + or -
+
+print(3 * 3 + 3 / 3 - 3) # ends up being (3*3) + (3/3) - 3 which is 9 + 1 - 3 = 7
+
+#Change above to get result of 3
+
+print(3 / 3 * 3 + 3 - 3) #moved operations around to get result of 3
+print(3 * (3 + 3) / 3 - 3) #This was the intended result I guess? To not move operators around but instead () the 3+3 to get the desired result of 3
