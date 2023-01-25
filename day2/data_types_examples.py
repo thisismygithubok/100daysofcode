2b1ab054a76607a25ebc7c85f732a1b10bbdd7af
Steven Schmidt <steven@thisismynameok.com>
2023-01-25 11:33:13 -0500

    adding data types examples

---
+++ day2/data_types_examples.py
@@ -0,0 +1,18 @@
+#Data Types
+
+#Strings
+print("Hello"[0]) #Selecting and printing "H"
+print("Hello"[4]) #Trying to select and print "o"
+print("123" + "345") #This just concatenates, doesn't add them
+
+#Integers
+print(123+345) #This is how you add integers, can't have them in "" as those denote strings
+print(123_456_789) #This is how you handle bigger numbers, if they're separated by commas
+
+#Float (decimals)
+3.14159
+
+#Boolean (True or False)
+True
+False
+
