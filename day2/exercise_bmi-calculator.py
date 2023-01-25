adaff7dc6fd7ac12d8d9ff50f6c8880f7b7b7b49
Steven Schmidt <steven@thisismynameok.com>
2023-01-25 12:39:21 -0500

    modify bmi calc to add in keeping the normal bmi number but then print the int version

--- day2/exercise_bmi-calculator.py
+++ day2/exercise_bmi-calculator.py
@@ -7,6 +7,7 @@ weight = input("How much do you weigh in kg? : ")
 int_weight = float(weight) #Takes the user's weight and floats, accepting decimals
 int_height = float(height) #Takes the user's height and floats, accepting decimals
 
-bmi = int(int_weight / (int_height**2)) #Calculates for BMI and converts to int, i.e. rounding down to the closest whole number
+bmi = (int_weight / (int_height**2)) #Caculates for BMI using provided user input numbers
+bmi_int = int(bmi) #Calculates for BMI and converts to int, i.e. rounding down to the closest whole number
 
-print(bmi) #Prints the result, rounded BMI number
+print(bmi_int) #Prints the result, rounded BMI number
