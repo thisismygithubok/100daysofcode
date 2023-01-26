241f8249cecb23a55e1248d35530990f64f56b8a
Steven Schmidt <steven@thisismynameok.com>
2023-01-26 15:42:02 -0500

    woops, couldn't use len() in average height - fixed it to be for loop

--- day5/exercise_average-height.py
+++ day5/exercise_average-height.py
@@ -7,13 +7,23 @@ for n in range(0, len(student_heights)): #For each student height in the list, b
 
 #Write your code below this row 👇
 
-#total_height = sum(student_heights)
-total_height = 0
+#total_height = sum(student_heights) - easier way to do this...
+total_height = 0 #Setting base height sum to 0
+count = 0 #Setting base num of students to 0
 
-for each_height in student_heights:
-    total_height = total_height + each_height
+for each_height in student_heights: #Iterating through the student_heights list
+    total_height = total_height + each_height #Gathers total height by iterating through the student_heights list and summing them by grabbing the previous run's value
     #Easier written as total_height += each_height
-total_height = total_height / len(student_heights)
-print(round(total_height))
+#print(round(total_height))
+
+for each_student in student_heights: #Iterating through the student heights list
+    count += 1 #Takes the base count of 0 and adds 1 for each list item it goes through, essentially counting how many list entries there are
+#print(count)
+
+average_height = round(total_height/count)
+print(average_height)
+
+#total_height = total_height / len(student_heights)
+
 
 
