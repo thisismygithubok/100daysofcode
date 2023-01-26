c052331fb0aa339649642080d59d0e760ffbb55d
Steven Schmidt <steven@thisismynameok.com>
2023-01-26 11:08:15 -0500

    add randomization example

---
+++ day4/example_randomization.py
@@ -0,0 +1,12 @@
+#Randomization
+
+import random
+
+random_integer = random.randint(1,10) #Random int between 1 and 10
+print(random_integer)
+
+random_float = random.random() * 5 #Random floating int between 1 and 5
+print(random_float)
+
+love_score = random.randint(1,100)
+print(f"Your love score is {love_score}")
