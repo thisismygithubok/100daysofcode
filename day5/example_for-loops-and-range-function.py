aa5caaf05984eada97e52e9fb01fe8cc16908695
Steven Schmidt <steven@thisismynameok.com>
2023-01-26 15:54:45 -0500

    add for loops with range example

---
+++ day5/example_for-loops-and-range-function.py
@@ -0,0 +1,12 @@
+#For loops with range
+
+total = 0
+
+for number in range(1,101): #final number has to be 1 bigger than the last num you want to iterate through
+    total += number
+
+print(total)
+
+for num in range (1, 11, 3): #3rd number is the size of the step
+    print(num)
+
