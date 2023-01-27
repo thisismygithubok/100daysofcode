cda5c0eaa670715221d368407c1434a87c48f91c
Steven Schmidt <steven@thisismynameok.com>
2023-01-27 16:14:28 -0500

    add defining and calling functions example, also custom functions

---
+++ day6/example_definiing-and-calling-functions.py
@@ -0,0 +1,14 @@
+#Functions
+
+print("Hello")
+num_char = len("Hello")
+print(num_char)
+
+#Making our own functions
+def my_function():
+    print("Hello")
+    print("Bye")
+
+my_function() #Calls our custom function
+
+ 
