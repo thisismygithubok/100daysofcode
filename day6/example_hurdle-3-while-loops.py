6a8c91fe97f0c4ce6bc300513e8d7ce8dbdd51bb
Steven Schmidt <steven@thisismynameok.com>
2023-01-27 17:38:47 -0500

    add hurdle 3 example using if statement in while loop

---
+++ day6/example_hurdle-3-while-loops.py
@@ -0,0 +1,21 @@
+##Example from hurdles 3 from reeborg.ca
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def hurdle():
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+while not at_goal():
+    if front_is_clear():
+        move()
+    elif wall_in_front():
+        hurdle()
