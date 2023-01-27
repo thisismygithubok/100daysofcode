f2485c8b6e36fa3c17af461011a6626f0d74e759
Steven Schmidt <steven@thisismynameok.com>
2023-01-27 16:24:42 -0500

    add hurdle jump example

--- day6/example_defining-and-calling-functions.py
+++ day6/example_defining-and-calling-functions.py
@@ -13,22 +13,22 @@ my_function() #Calls our custom function
 
 #Performed some more examples over at https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=%2Fworlds%2Fmenus%2Fselect_collection_en.json&name=Alone&url=%2Fworlds%2Ftutorial_en%2Falone.json
 
-# def turn_around():
-#     turn_left()
-#     turn_left()
+def turn_around():
+    turn_left()
+    turn_left()
     
-# def turn_right():
-#     turn_left()
-#     turn_left()
-#     turn_left()
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
 
-# #Draw Square
-# turn_left()
-# move()
-# turn_right()
-# move()
-# turn_right()
-# move()
-# turn_right()
-# move()
-# turn_around()
+#Draw Square
+turn_left()
+move()
+turn_right()
+move()
+turn_right()
+move()
+turn_right()
+move()
+turn_around()
