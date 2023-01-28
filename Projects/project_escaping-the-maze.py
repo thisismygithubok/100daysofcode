f9ecfddb2aed56ffe9431936e7db40d4a2184f6d
Steven Schmidt <steven@thisismynameok.com>
2023-01-28 16:43:39 -0500

    add hangman exercise, also cleanup 2 day 6 files

--- Projects/project_escaping-the-maze.py
+++ Projects/project_escaping-the-maze.py
@@ -1,20 +1,20 @@
 #Maze from reeborg.ca - DID NOT GET THIS AT ALL, SO COMPLICATED, DID NOT UNDERSTAND THE LOGIC
 
-def turn_right():
-    turn_left()
-    turn_left()
-    turn_left()
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
 
-while front_is_clear():
-    move()
-turn_left()
+# while front_is_clear():
+#     move()
+# turn_left()
 
 
-while not at_goal():
-    if right_is_clear():
-        turn_right()
-        move()
-    elif front_is_clear():
-        move()
-    else:
-        turn_left()
+# while not at_goal():
+#     if right_is_clear():
+#         turn_right()
+#         move()
+#     elif front_is_clear():
+#         move()
+#     else:
+#         turn_left()
